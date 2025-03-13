1eb79b0d99a246d6084637a92b1ab3227786e443
Matheus Souza <125572561+matheusroberto04@users.noreply.github.com>
2025-03-13 14:12:47 -0300

    MaxGames
    Projeto desenvolvido em conjunto com a Alura

---
+++ MaxGames/js/app.js
@@ -0,0 +1,48 @@
+let jogosAlugados = 0;
+let jogosDevolvidos = 0;
+
+function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+function contarEExibirJogosDevolvidos(){
+    console.log(`Total de jogos devolvidos: ${jogosDevolvidos}`)
+}
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if(confirm(`Você confirma a devolução desse jogo ${nomeJogo.textContent}?`)){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+        jogosAlugados--;
+        jogosDevolvidos++;
+        }
+    } else {
+        if(confirm(`Você deseja alugar o jogo ${nomeJogo.textContent}?`))
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+        jogosAlugados++;
+        jogosDevolvidos--;
+    }
+
+
+    contarEExibirJogosAlugados();
+    contarEExibirJogosDevolvidos();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    jogosDevolvidos = document.querySelectorAll('.dashboard__item__button--return').length;
+    contarEExibirJogosDevolvidos
+})
